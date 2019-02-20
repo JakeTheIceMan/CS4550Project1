6897e58641c5372d02ae8abe706563b80549f0f0
Ben Romine <elementnumber35@gmail.com>
2019-02-20 15:55:26 -0500

    Commenting & formatting

--- lib/othello_web/channels/games_channel.ex
+++ lib/othello_web/channels/games_channel.ex
@@ -1,14 +1,13 @@
 defmodule OthelloWeb.GamesChannel do
   use OthelloWeb, :channel
-
   alias Othello.GameServer
 
   intercept ["update"]
 
+  # Join a game.
   def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
-      socket = socket
-      |> assign(:name, name)
+      socket = assign(socket, :name, name)
       GameServer.start(name)
       {:ok, %{"join" => name, "game" => GameServer.view(name)}, socket}
     else
@@ -16,25 +15,28 @@ defmodule OthelloWeb.GamesChannel do
     end
   end
 
+  # Allow a given player to make a move in a game.
   def handle_in("choose", %{"row" => r, "column" => c}, socket) do
     game = GameServer.choose(socket.assigns[:name], r, c, socket.assigns[:user])
     update!(game, socket)
     {:reply, {:ok, %{"game" => game}}, socket}
   end
 
+  # Restart a game.
   def handle_in("restart", _, socket) do
     game = GameServer.restart(socket.assigns[:name])
     update!(game, socket)
     {:reply, {:ok, %{"game" => game}}, socket}
   end
 
+  # Format the updated push message.
   def handle_out("update", game, socket) do
-    IO.inspect("Broadcasting update to #{socket.assigns[:user]}")
     push(socket, "update", %{"game" => game})
     {:noreply, socket}
   end
 
-  def update!(game, socket) do
+  # Private function that broadcasts the game state to all users.
+  defp update!(game, socket) do
     broadcast!(socket, "update", game)
   end
 
--- lib/othello_web/controllers/page_controller.ex
+++ lib/othello_web/controllers/page_controller.ex
@@ -5,17 +5,24 @@ defmodule OthelloWeb.PageController do
     render(conn, "index.html")
   end
 
+  # Set up a user to join a given game.
   def join(conn, %{"join" => %{"user" => user, "name" => name}}) do
+    # Put the user in the current session, and redirect them to the game.
     conn
     |> put_session(:user, user)
     |> redirect(to: "/game/#{name}")
   end
 
+  # Show a game state.
   def game(conn, params) do
     user = get_session(conn, :user)
+    # If a user is present,
     if user do
+      # Render the game for the given user.
       render conn, "game.html", name: params["name"], user: user
+    # Otherwise,
     else
+      # Tell the user to pick a color and redirect them to the index.
       conn
       |> put_flash(:error, "Please pick a color.")
       |> redirect(to: "/")
