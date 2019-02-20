e12a3c132417cc00a8fc7fdd7b9fb5bf4fb801f8
Ben Romine <elementnumber35@gmail.com>
2019-02-20 12:29:35 -0500

    Commenting

--- assets/js/othello.jsx
+++ assets/js/othello.jsx
@@ -10,11 +10,12 @@ class Starter extends React.Component {
   constructor(props) {
     super(props);
     this.channel = props.channel;
+    // Set the state of the board.
     this.state = {
       black_turn: true,
       board: []
     };
-
+    // Attempt to join the game.
     this.channel
     .join()
     .receive("ok", resp => {
@@ -24,26 +25,28 @@ class Starter extends React.Component {
     .receive("error", resp => {
       console.log("Unable to join", resp);
     });
-
+    // Whenever the channel updates, set the state.
     this.channel.on("update", resp => {
       this.setState(resp.game);
     });
   }
 
-  //n
+  // Call this function to restart the game.
   restart() {
     this.channel.push("restart")
     .receive("ok", resp => { this.setState(resp.game); });
   }
 
+  // Call this function to choose a tile.
   choose(r, c) {
     this.channel
     .push("choose", {row: r, column: c})
     .receive("ok", resp => { this.setState(resp.game); });
   }
 
-  //r
+  // Render the board.
   render() {
+    // Render each of the rows in the board.
     let board = _.map(this.state.board, (row, rowIndex) => {
       return <ShowRow
       key={rowIndex}
@@ -52,27 +55,31 @@ class Starter extends React.Component {
       choose={this.choose.bind(this)}
       row={row} />;
     });
-
+    // This is the restart button.
     let restart = (
       <div className="column">
       <button onClick={this.restart.bind(this)}>Restart</button>
       </div>
     );
+    // This displays the winner.
     let winner = (
       <div className="column">
       <p>The winner is {this.state.winner}!</p>
       </div>
     );
+    // This displays whose turn it is.
     let turn = (
       <div className="column">
       <h4>It is now {this.state.black_turn ? "black" : "white"}'s turn.</h4>
       </div>
     );
-
+    // Return the HTML.
     return (
       <div>
       <div className="row">
+      {/* If we have no winner, display whose turn it is, otherwise show the restart button. */}
       {this.state.winner === "none" ? turn : restart}
+      {/*If we have no winner, don't display anything, otherwise show who won. */}
       {this.state.winner === "none" ? null : winner}
       </div>
       {board}
@@ -81,8 +88,12 @@ class Starter extends React.Component {
   }
 }
 
+// Render a row in the board.
 function ShowRow(props) {
+  // Render each tile in the row.
   let renderedRow = _.map(props.row, (col, colIndex) => {
+    // Return a div with a class name equal to its color.
+    // If the div is clicked, indicate that the tile has been chosen.
     return (
       <div className="column" key={colIndex}>
       <div className={col.color} onClick={
@@ -91,6 +102,6 @@ function ShowRow(props) {
       </div>
     )
   });
-
+  // Return the HTML.
   return <div className="row">{renderedRow}</div>;
 }
--- lib/othello/backup_agent.ex
+++ lib/othello/backup_agent.ex
@@ -1,16 +1,19 @@
 defmodule Othello.BackupAgent do
   use Agent
 
+  # Start the backup agent.
   def start_link(_args) do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
+  # Store something in the backup agent.
   def put(name, val) do
     Agent.update __MODULE__, fn state ->
       Map.put(state, name, val)
     end
   end
 
+  # Retrieve something from the backup agent.
   def get(name) do
     Agent.get __MODULE__, fn state ->
       Map.get(state, name)
